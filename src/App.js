8b7fd582d4a6687a039fab4bae9f91a27b761167
Olawale Orija <waleob@googlemail.com>
2023-03-03 15:43:27 +0000

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
+
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="1ca413b8-0ad2-4fd4-a9da-1d1ad06240b4"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} /> }
+        />
+    );
 }
 
 export default App;
---
+++ src/components/MyMessage.jsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+
+
+const MyMessage = ({ message }) => {
+     
+    if(message.attachments.length > 0){
+        return (
+            <img
+                src={message.attachments[0].file}
+                alt="message-attachment"
+                className="message-image"
+                style={{ float: 'right' }}
+            />
+
+        );
+    
+    }
+    return(
+        <div className="message" style={{ float: 'right', marginright: '18px', color: 'white', backgroundColor: '#42474D'}}>
+            {message.text}
+        </div>
+    );
+
+}
+export default MyMessage;
